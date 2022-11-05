997ff5f621186f0a754b630fcdde862112e05b64
Jan Michálek <janmichalek213@gmail.com>
2022-11-05 13:28:53 +0100

    correction of translations

--- languages/cs.php
+++ languages/cs.php
@@ -1,90 +1,36 @@
 <?php
     $langNav = array(
-        /* Horni menu */
+        /* HEADER */
+        "fullName" => "JAN MICHÁLEK",
+        "specialization" => "JUNIOR FRONT-END PROGRAMÁTOR",
+        /* LEFT SIDE */
         "personal" => "OSOBNÍ INFORMACE",
         "name" => "Jan Michálek",
-        "moje prace" => "Moje práce",
-        "cenik" => "Ceník",
-        "ahoj" => "Ahoj, ",
-        "jmenuji se" => " jmenuji se ",
-        "honza" => "Honza",
-        "jsem" => "Jsem nezávislý webový vývojář, který pomáhá svým klientům <br> se zviditelnit na internetu.",
-        "moje sluzby" => "Moje služby",
+        "contact" => "KONTAKT",
+        "phone" => "734 126 757",
+        "links" => "SÍTĚ",
 
-        /* Box se sluzbama */
-        "tvorba webovych stranek" => "Tvorba webových stránek",
-        "webova prezentace" => "Webová prezentace je dnes základním kamenem pro úspěšnou prezentaci Vaší firmy na internetu. Pomohu Vám vytvořit kvalitní a uživatelský přivětivý web.",
-        "tvorba loga a ikon" => "Tvorba loga a ikon",
-        "kvalitni logo" => "Kvalitní logo by se mělo člověku okamžitě vrýt do paměti na první pohled. Mělo by být jednoduché, výrazné, snadno zapamatovatelné. Já Vám takové logo pomohu vytvořit.",
-        "sprava databaze" => "Správa databáze",
-        "pokud pouzivate dtb" => "Pokud používáte pro správu vlastní databáze relační databázi MySQL, tak se na mne neváhejte obrátit. Mohu Vám pomoct provést úpravy ve stavajcí MySQL databázi nebo Vám vytvořit zcela novou databázi.",
-        "sprava vaseho" => "Správa vašeho eshopu",
-        "pokud pouzivate" => "Pokud používáte pro správu svého eshopu krabicové řešení od společnosti Shoptet, tak se neváhejte na mě obrátit. Pomůžu Vám s grafikou, nastavením i SEO.",
-        
-        /* Box moje prace 1*/
-        "jak muze vypadat" => "Jak může váš web vypadat",
-        "tento web slouzi 0" => "Tento web BESTBURGER slouží jako ukázka mé práce. Prezentuje objednávku jídla, resp. burgerů.",
-        "funkcnost webu" => "Funkčnost webu:",
-        "na webu si 0" => "Na webu si lze vybrat jeden ze tří burgerů a následně si zvolit sílu pálivé omáčky. Burger lze přidat do košíku.",
-        "programovano pomoci" => "Programováno pomocí:",
-        "jazyky" => "HTML, CSS, JavaScript",
-        "navstivit web" => "NAVŠTÍVIT WEB",
-
-        /* Box moje prace 2*/
-        "tento web slouzi 1" => "Tento web WATERBOTTLE slouží jako ukázka mé práce. Ukázka představuje web pro prezentaci produktu - láhve na vodu. Design webu je v modernejším kabátku.",
-        "na webu si 1" => "Na webu lze používat tzv. hamburger menu jak v mobilní tak i v deskoptové verzi.",
-    
-        /* Ceník */
-        "cenik" => "Ceník",
-        "tvorba webovych stranek" => "Tvorba webových stránek",
-        "tw 0" => "Tvorba čistého kódu",
-        "tw 1" => "Responzivní design",
-        "tw 2" => "Kvalitní SEO",
-        "tw 3" => "Firemní prezentace",
-        "tw 4" => "Tvorba redakčního systému",
-        "tw 5" => "Tvorba blogu",
-        "tw cena" => "od 2500Kč,-",
-        "tvorba loga" => "Tvorba loga a ikon",
-        "tl 0" => "Logo manuál",
-        "tl 1" => "Jedinečná a neomezená licence",
-        "tl 2" => "Redesign loga nebo ikon",
-        "tl 3" => "Možnost nespočtu úprav",
-        "tl 4" => "Odliším Vás od konkurence",
-        "tl 5" => "Tvorba minimálně 6 ikon",
-        "tl cena" => "od 1500Kč,-",
-        "sprava databaze" => "Správa databáze",
-        "sd 0" => "Vytvoření MySQL příkazů",
-        "sd 1" => "Práce s Triggery",
-        "sd 2" => "Rutiny",
-        "sd 3" => "Vytváření vazeb",
-        "sd 4" => "Optimalizace výkonu databáze",
-        "sd 5" => "Fulltextové vyhledávání",
-        "sd cena" => "od 400Kč,-/hod",
-        "sprava eshopu" => "Správa vašeho eshopu",
-        "se 0" => "Nastavení domény",
-        "se 1" => "Nastavení XML feedu",
-        "se 2" => "SEO",
-        "se 3" => "Vkládání a úprava produktů",
-        "se 4" => "Grafická úprava šablony",
-        "se 5" => "Celková analýza eshopu",
-        "se cena" => "od 400Kč,-/hod",
-
-        "vsechny ceny" => "Všechny ceny jsou uvedeny bez DPH.",
-        "ceny jsou orientacni" => "Ceny jsou orientační. Pro vytvoření cenové nabídky mě můžete nezávazně kontaktovat.",
-
-        /* Kontakt */
-        "kontakt" => "Kontakt",
-        "jmeno" => "Jméno",
-        "email" => "Email",
-        "telefon" => "Telefon",
-        "zde napiste zpravu" => "Zde mi napiště zprávu...",
-        "odeslat zpravu" => "Odeslat zprávu"
-    );
-
-    /* Zakazano kvuli Onebit - nefunguji zde 2D Session */
-    /* if(empty($_SESSION['dtb']['cs'])){
-        $JSON =  json_encode($langNav, JSON_PRETTY_PRINT);
-        file_put_contents('./languages/multilanguagecs.json', $JSON);
-        $_SESSION['dtb']['cs'] = 1;
-    }*/
+        /* RIGHT SIDE */
+        "profile" => '
+                    <p>Ahoj, <br /><br />
+                    Jsem vášnivý samouk front-end webový vývojář a programování je mou vášní už od střední školy. Poslední rok a půl se velmi aktivně učím programovat na frontendu JavaScript, TypeScript a nedávno jsem se ponořil do Reactu a React native, ve kterém bych rád v budoucnu tvořil webové i mobilní aplikace. <br /><br /> Abych se více zdokonalil v programování tak aktuálně dokončují rekvalifikační kurz vývojář www aplikací u <a href="https://www.itnetwork.cz/">ITNetwork</a> a také ať mám aspoň nějaký ten papír v ruce. Po práci v elektro oboru, hledám svou první pracovní příležitost jako front-end junior programátor.
+                    <br /><br />
+                    Pro svou lepší prezentaci ať nezůstanu jen u pouhých slov jsem si pro Vás připravil ukázku své práce. Navrhl jsem jednoduchou webovou aplikaci se základními funkcemi a správou SQL databázi pro pojišťovnictví. Jedná se o single-page aplikaci, která pro veškerou práci s DOM využívá vanilla JS.
+                    <a href="https://www.michalekjan.cz/insuranceApp/" class="js-app">UKÁZKA</a>
+                    Pokud jsem Vás aspoň trochu zaujal, tak mne prosím kontaktujte. Velmi rád bych se s vámi setkal.</p>
+        ',
+        "skills" => "DOVEDNOSTI",
+        "work" => "PRACOVNí ZKUŠENOSTI",
+        "date" => "2022 - souč.",
+        "projectManager" => "Projektový manager",
+        "education" => "VZDĚLÁNÍ",
+        "school" => "Střední škola polytechnická, Havířov-Šumbark",
+        "fielOfEducation" => "Elektrotechnik - maturita",
+        "hobbies" => "ZÁJMY",
+        "sport" => "Sport",
+        "books" => "knihy",
+        "programming" => "Programování",
+        "games" => "Hry",
+        "movies" => "Filmy"
+    )
 ?>
--- languages/en.php
+++ languages/en.php
@@ -1,90 +1,36 @@
 <?php
     $langNav = array(
-        /* Horni menu */
+        /* HEADER */
+        "fullName" => "JAN MICHÁLEK",
+        "specialization" => "JUNIOR FRONT-END DEVELOPER",
+        /* LEFT SIDE */
         "personal" => "PERSONAL",
         "name" => "Jan Michálek",
-        "moje prace" => "My work",
-        "cenik" => "Price",
-        "ahoj" => "Hello, ",
-        "jmenuji se" => " my name is ",
-        "honza" => "Jan",
-        "jsem" => "I am an independent web developer who helps my clients <br> become visible on the Internet.",
-        "moje sluzby" => "My services",
+        "contact" => "CONTACT",
+        "phone" => "+420 734 126 757",
+        "links" => "LINKS",
 
-        /* Box se sluzbama */
-        "tvorba webovych stranek" => "Website building",
-        "webova prezentace" => "Today, a website is the cornerstone of a successful presentation of your company on the Internet. I will help you create a quality and user-friendly website.",
-        "tvorba loga a ikon" => "Logo and icon design",
-        "kvalitni logo" => "A quality logo should be immediately remembered at first sight. It should be simple, expressive, easy to remember. I will help you create such a logo.",
-        "sprava databaze" => "Database administration",
-        "pokud pouzivate dtb" => "If you use a MySQL relational database to manage your own database, do not hesitate to contact me. I can help you make modifications to the existing MySQL database or create a completely new database.",
-        "sprava vaseho" => "Manage your eshop",
-        "pokud pouzivate" => "If you use a box solution from Shoptet to manage your e-shop, do not hesitate to contact me. I will help you with graphics, settings and SEO.",
-        
-        /* Box moje prace 1*/
-        "jak muze vypadat" => "What your site can look like",
-        "tento web slouzi 0" => "This BESTBURGER website serves as an example of my work. Presents a food order, resp. burgers.",
-        "funkcnost webu" => "Website functionality:",
-        "na webu si 0" => "You can choose one of the three burgers on the website and then choose the strength of the hot sauce. Burger can be added to cart.",
-        "programovano pomoci" => "Programmed using:",
-        "jazyky" => "HTML, CSS, JavaScript",
-        "navstivit web" => "VISIT THE WEB",
-
-        /* Box moje prace 2*/
-        "tento web slouzi 1" => "Tento web WATERBOTTLE slouží jako ukázka mé práce. Ukázka představuje web pro prezentaci produktu - láhve na vodu. Design webu je v modernejším kabátku.",
-        "na webu si 1" => "Na webu lze používat tzv. hamburger menu jak v mobilní tak i v deskoptové verzi.",
-    
-        /* Ceník */
-        "cenik" => "Price list",
-        "tvorba webovych stranek" => "Website creation",
-        "tw 0" => "Clean code",
-        "tw 1" => "Responsive design",
-        "tw 2" => "Quality SEO",
-        "tw 3" => "Company presentation",
-        "tw 4" => "Creation of a content management system",
-        "tw 5" => "Creating a blog",
-        "tw cena" => "from 120$",
-        "tvorba loga" => "Creation of logos and icons",
-        "tl 0" => "Logo manual",
-        "tl 1" => "Unique and unlimited license",
-        "tl 2" => "Redesign of logos or icons",
-        "tl 3" => "Possibility of countless adjustments",
-        "tl 4" => "I will set you apart from the competition",
-        "tl 5" => "Creating at least 6 icons",
-        "tl cena" => "from 80$",
-        "sprava databaze" => "Database administration",
-        "sd 0" => "Creating MySQL statements",
-        "sd 1" => "Working with Triggers",
-        "sd 2" => "Routines",
-        "sd 3" => "Creating links",
-        "sd 4" => "Database performance optimization",
-        "sd 5" => "Full-text search",
-        "sd cena" => "from 15$ / hour",
-        "sprava eshopu" => "Manage your e-shop",
-        "se 0" => "Domain settings",
-        "se 1" => "XML feed settings",
-        "se 2" => "SEO",
-        "se 3" => "Inserting and editing products",
-        "se 4" => "Graphic design template",
-        "se 5" => "Overall analysis of the eshop",
-        "se cena" => "from 15$ / hour",
-
-        "vsechny ceny" => "All prices are without VAT.",
-        "ceny jsou orientacni" => "Prices are indicative. You can contact me without obligation to create a price offer.",
-
-        /* Kontakt */
-        "kontakt" => "Contact",
-        "jmeno" => "Name",
-        "email" => "Email",
-        "telefon" => "Phone",
-        "zde napiste zpravu" => "Write me a message here...",
-        "odeslat zpravu" => "Send a message"
-    );
-
-     /* Zakazano kvuli Onebit - nefunguji zde 2D Session */
-    /* if(empty($_SESSION['dtb']['en'])){
-        $JSON =  json_encode($langNav, JSON_PRETTY_PRINT);
-        file_put_contents('./languages/multilanguageen.json', $JSON);
-        $_SESSION['dtb']['en'] = 1;
-    } */
+        /* RIGHT SIDE */
+        "profile" => '
+                    <p>Hi, <br /><br />
+                    i&apos;m an avid self-taught front-end web developer and programming has been a passion of mine since high school. For the last year and a half, I have been actively learning front-end programming in JavaScript, TypeScript, and recently I immersed myself in React and React native, in which I would like to create web and mobile applications in the future. <br /><br /> In order to improve my programming skills, I am currently completing a web application developer retraining course at <a href="https://www.itnetwork.cz/">ITNetwork</a> and also to have at least some paper in hands. After working in the electrical field, I am looking for my first job opportunity as a front-end junior programmer.
+                    <br /><br />
+                    For a better presentation, I have prepared a sample of my work for you. I designed a simple web application with basic functions and SQL database management for the insurance industry. It is a single-page application that uses vanilla JS for all work with the DOM.
+                    <a href="https://www.michalekjan.cz/insuranceApp/" class="js-app">SAMPLE</a>
+                    If I have interested you at least a little bit, please contact me. I would very much like to meet with you.</p>
+        ',
+        "skills" => "SKILLS",
+        "work" => "WORK",
+        "date" => "2022 - souč.",
+        "projectManager" => "Project manager",
+        "education" => "EDUCATION",
+        "school" => "Secondary Polytechnic School, Havířov-Šumbark",
+        "fielOfEducation" => "Electrical engineer - high school diploma",
+        "hobbies" => "HOBBIES   ",
+        "sport" => "Sport",
+        "books" => "Books",
+        "programming" => "Programming",
+        "games" => "Games",
+        "movies" => "Movies"
+    )
 ?>
